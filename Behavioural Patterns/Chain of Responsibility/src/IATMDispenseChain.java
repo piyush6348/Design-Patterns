5081bbe20280044d66e1f37505637f555b96ae25
angroene <44263448+angroene@users.noreply.github.com>
2021-09-27 15:02:14 +0200

    Add nextChain to cater for NullPointerException
    Add nextChain(int) method that will perform a null check before executing the next chain. This is to avoid NullPointerException when amount is less than 10.

--- Behavioural Patterns/Chain of Responsibility/src/IATMDispenseChain.java
+++ Behavioural Patterns/Chain of Responsibility/src/IATMDispenseChain.java
@@ -6,4 +6,12 @@ public abstract class IATMDispenseChain {
     }
 
     public abstract void dispense(int amount);
+    
+    public void nextChain(int amount) {
+        if (this.nextChain != null) {
+            this.nextChain.dispense(amount);
+        } else {
+            System.out.println("No notes can be dispensed");
+        }
+    }
 }
